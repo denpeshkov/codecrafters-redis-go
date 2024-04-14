a825090a00212423e7331226d5d9e42e0b94b3e5
Denis Peshkov <denpeshkov98@gmail.com>
2024-04-14 22:44:43 +0400

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -7,14 +7,31 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+	if err := run(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+}
+
+func run() error {
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return fmt.Errorf("bind port 6379: %w", err)
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		return fmt.Errorf("accepting connection: %w", err)
+	}
+
+	b := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(b)
+		if err != nil {
+			return fmt.Errorf("reading from connection: %w", err)
+		}
+		fmt.Println(string(b[:n]))
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
