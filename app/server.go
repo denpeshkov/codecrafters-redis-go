7fee9df35f641e941252aeeb7786e1fe393d05f7
Denis Peshkov <denpeshkov98@gmail.com>
2024-04-15 10:42:52 +0400

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -9,7 +10,7 @@ import (
 
 func main() {
 	if err := run(); err != nil {
-		fmt.Println(err)
+		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 }
@@ -20,17 +21,26 @@ func run() error {
 		return fmt.Errorf("bind port 6379: %w", err)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		return fmt.Errorf("accepting connection: %w", err)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return fmt.Errorf("accepting connection: %w", err)
+		}
+		go handleConn(conn)
 	}
+}
 
+func handleConn(conn net.Conn) {
 	b := make([]byte, 1024)
 	for {
 		n, err := conn.Read(b)
-		if err != nil && err != io.EOF {
-			return fmt.Errorf("reading from connection: %w", err)
+		if errors.Is(err, io.EOF) {
+			return
 		}
+		if err != nil {
+			fmt.Println("Reading from connection: ", err)
+		}
+
 		fmt.Printf("received: %q\n", string(b[:n]))
 		conn.Write([]byte("+PONG\r\n"))
 	}
